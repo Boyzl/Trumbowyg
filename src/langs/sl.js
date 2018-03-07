dcaeeb4431ca585ab8336f655a1eb76b2c1d0c4d
Boyzl <167259+Boyzl@users.noreply.github.com>
2018-03-07 13:03:09 +0100

    sl.js
    slovenian translations

---
+++ src/langs/sl.js
@@ -0,0 +1,9 @@
+/* ===========================================================
+ * sl.js
+ * Slovenian translation for Trumbowyg
+ * http://alex-d.github.com/Trumbowyg
+ * ===========================================================
+ * Author : Bojan Pesek
+ *          Github : https://github.com/Boyzl
+ */
+jQuery.trumbowyg.langs.sl={viewHTML:"Prikaži HTML",undo:"Razveljavi",redo:"Uveljavi",formatting:"Oblikovanje",p:"Odstavek",blockquote:"Navedek",code:"Koda",header:"Naslov",bold:"Krepko",italic:"Ležeče",strikethrough:"Prečrtano",underline:"Podčrtano",strong:"Pomembno",em:"Poudarjeno",del:"Brisano",superscript:"Nadpisano",subscript:"Podpisano",unorderedList:"Označeni seznam",orderedList:"Oštevilčeni seznam",insertImage:"Vstavi sliko",link:"Povezava",createLink:"Vstavi povezavo",unlink:"Odstrani povezavo",justifyLeft:"Leva poravnava",justifyCenter:"Sredinska poravnava",justifyRight:"Desna poravnava",justifyFull:"Obojestranska poravnava",horizontalRule:"Vstavi vodoravno črto",removeformat:"Odstrani oblikovanje",fullscreen:"Celozaslonsko",close:"Zapri",submit:"Potrdi",reset:"Prekliči",required:"Zahtevano",description:"Opis",title:"Naslov",text:"Text",target:"Cilj",width:"Širina"};
